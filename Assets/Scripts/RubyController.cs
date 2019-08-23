ff3bed7a126e812d733a9fe8644663cfddd8f324
acsexton <andrew@acsexton.com>
2019-08-23 18:15:20 -0400

    added work from ~first half of tutorial

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+
+    public float moveSpeed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    int currentHealth;
+    public int health { get { return currentHealth; } }
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+
+    Animator animator;
+    Vector2 lookDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+
+        animator = GetComponent<Animator>();
+        Vector2 lookDirection = new Vector2(0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        // Approximately rather than equality to account for float rounding problems
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        Vector2 position = rigidbody2d.position;
+        position = position + move * moveSpeed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        
+        if (amount < 0)
+        {
+            animator.SetTrigger("Hit");
+
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        // Clamp keeps first param between 2nd and 3rd params
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
